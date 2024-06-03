5eb6937a000324aeb4031b4f4bcbd2b83828b6d6
Baibhavi <baibhavipandey@gmail.com>
2024-06-03 11:38:26 +0530

    authentication done

---
+++ auth.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD2VM9CAeoIIPFZy6lk7wgkwc7klzl6iIU",
+    authDomain: "worthit-login.firebaseapp.com",
+    projectId: "worthit-login",
+    storageBucket: "worthit-login.appspot.com",
+    messagingSenderId: "886304694412",
+    appId: "1:886304694412:web:1773fc7af00677a618dba9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
